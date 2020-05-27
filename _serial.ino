68fed512d364803c622623ebc509165dc05bb8b6
Zeron <zeron@zeron.ca>
2020-05-26 17:32:46 -0700

    Support Fuses from AVRDUDE
    Allow AVRDUDE to read and write fuses.

--- _serial.ino
+++ _serial.ino
@@ -74,6 +74,36 @@ void universal() {
     writeHV(0, 0b01100100);         //Chip Erase
     writeHV(0, 0b01101100);         //Chip Erase
     breply(0);  //AVRDUDE expects a reply...  Just send a 0
+  } else if (buff[0] == 0x58 && buff[1] == 0x08 && buff[2] == 0 && buff[3] == 0){ //Read High Fuse
+    writeHV(0x04, 0x4C);
+    writeHV(0x00, 0x7A);
+    breply(writeHV(0x00, 0x7E));
+  } else if (buff[0] == 0xAC && buff[1] == 0xA8 && buff[2] == 0){ //Write High Fuse - buff[3] contains value
+    writeHV(0x40, 0x4C);
+    writeHV(buff[3], 0x2C);
+    writeHV(0x00, 0x74);
+    writeHV(0x00, 0x7C);
+    breply(0);
+  } else if (buff[0] == 0x50 && buff[1] == 0 && buff[2] == 0 && buff[3] == 0){ //Read Low Fuse
+    writeHV(0x04, 0x4C);
+    writeHV(0x00, 0x68);
+    breply(writeHV(0x00, 0x6C));
+  } else if (buff[0] == 0xAC && buff[1] == 0xA0 && buff[2] == 0){ //Write Low Fuse - buff[3] contains value
+    writeHV(0x40, 0x4C);
+    writeHV(buff[3], 0x2C);
+    writeHV(0x00, 0x64);
+    writeHV(0x00, 0x6C);
+    breply(0);
+  } else if (buff[0] == 0x50 && buff[1] == 0x08 && buff[2] == 0 && buff[3] == 0){ //Read Ext Fuse
+    writeHV(0x04, 0x4C);
+    writeHV(0x00, 0x6A);
+    breply(writeHV(0x00, 0x7C));
+  } else if (buff[0] == 0xAC && buff[1] == 0xA4 && buff[2] == 0){ //Write Ext Fuse - buff[3] contains value
+    writeHV(0x40, 0x4C);
+    writeHV(buff[3], 0x2C);
+    writeHV(0x00, 0x66);
+    writeHV(0x00, 0x6C);
+    breply(0);
   }
   #ifdef saveDebugToEEPROM
     else {
